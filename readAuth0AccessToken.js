ff0861dca9414af57ff94c55ec75b77fd94b0f08
madhumitha-github <madhu.22.mitha@gmail.com>
2018-02-27 12:24:15 +0530

    Update readAuth0AccessToken.js

--- readAuth0AccessToken.js
+++ readAuth0AccessToken.js
@@ -4,9 +4,8 @@ var appServer = express();
 var port = process.env.PORT || 8080;
 
 appServer.get('/', function(serverRequest, serverResponse) {    
-    
+	
 	var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL21hZGh1LWF1dGguYXV0aDAuY29tLyIsInN1YiI6IjhtMkF3YXMyZ1djQVNLaTdzNW5VM3ZPM2V4VVN0M0hFQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2FsZXhhLWJhbmsuYXBpIiwiaWF0IjoxNTE5NzA5ODQ3LCJleHAiOjE1MTk3OTYyNDcsImF6cCI6IjhtMkF3YXMyZ1djQVNLaTdzNW5VM3ZPM2V4VVN0M0hFIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.iduPOGHeo2DPCtKZaBuNqh94xVudalMOAjPRDkj6LcI';
-
 	jwt.verify(token, 'KPYlY29oD3mabd4NSXJZP4QrLqtzdZah', function(error, decodedPayload) {
 		if(error) {
 			return serverResponse.send('Access Token not authorized');
@@ -14,6 +13,24 @@ appServer.get('/', function(serverRequest, serverResponse) {
 		    	return serverResponse.send(decodedPayload);			
 		}
 	});
+});
+
+appServer.get('/getAccessToken', function(serverRequest, serverResponse) {  
+
+	var token;
+	
+	var options = { method: 'POST',
+	  url: 'https://madhu-auth.auth0.com/oauth/token',
+	  headers: { 'content-type': 'application/json' },
+	  body: '{"client_id":"8m2Awas2gWcASKi7s5nU3vO3exUSt3HE","client_secret":"wE3tuyod3g-Sfs78424U3nEPyC4pPofpKUnOhWNjS_gM3kgydSLGf6oQIUN28_My","audience":"https://alexa-bank.api","grant_type":"client_credentials"}' };
+
+	serverRequest(options, function (error, response, body) {
+	  token = body;
+	  console.log(body);
+	});
+	
+	return serverResponse.send(token);
+	
 });
 	      
 appServer.listen(port, function() {
