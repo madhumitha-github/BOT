52a2d988182d43ff1dbb60259e88aa8c8f152cf5
madhumitha-github <madhu.22.mitha@gmail.com>
2018-02-27 11:45:31 +0530

    Update readAuth0AccessToken.js

--- readAuth0AccessToken.js
+++ readAuth0AccessToken.js
@@ -7,7 +7,7 @@ appServer.get('/', function(serverRequest, serverResponse) {
     
 	var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL21hZGh1LWF1dGguYXV0aDAuY29tLyIsInN1YiI6IjhtMkF3YXMyZ1djQVNLaTdzNW5VM3ZPM2V4VVN0M0hFQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2FsZXhhLWJhbmsuYXBpIiwiaWF0IjoxNTE5NzA5ODQ3LCJleHAiOjE1MTk3OTYyNDcsImF6cCI6IjhtMkF3YXMyZ1djQVNLaTdzNW5VM3ZPM2V4VVN0M0hFIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.iduPOGHeo2DPCtKZaBuNqh94xVudalMOAjPRDkj6LcI';
 
-	jwt.verify(token, '', function(error, decodedPayload) {
+	jwt.verify(token, 'KPYlY29oD3mabd4NSXJZP4QrLqtzdZah', function(error, decodedPayload) {
 		if(error) {
 			return serverResponse.send(decodedPayload + 'Access Token not authorized');
 		} else {
